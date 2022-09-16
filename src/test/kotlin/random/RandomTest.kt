bafef412c5a52b73f0019137d828858b81410d45
junuu <bababrll@naver.com>
2022-09-16 15:29:38 +0900

    test: random 3번 연속 호출 테스트 추가 및 SecureRandom 학습 테스트 제거

--- src/test/kotlin/random/RandomTest.kt
+++ src/test/kotlin/random/RandomTest.kt
@@ -2,7 +2,6 @@ package random
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import java.security.SecureRandom
 
 class RandomTest {
 
@@ -19,6 +18,22 @@ class RandomTest {
     }
 
     @Test
+    fun `랜덤으로 1~9까지의 숫자를 3번 생성할 수 있다`() {
+        //given
+        val generateRandomNumber = GenerateRandomNumberForTest(5)
+
+        //when
+        val result1 = generateRandomNumber.getRandomNumberAndValidation()
+        val result2 = generateRandomNumber.getRandomNumberAndValidation()
+        val result3 = generateRandomNumber.getRandomNumberAndValidation()
+
+        //then
+        Assertions.assertEquals(result1, 5)
+        Assertions.assertEquals(result2, 5)
+        Assertions.assertEquals(result3, 5)
+    }
+
+    @Test
     fun `1~9까지의 수가 아니라면 IllegalArgumentException()이 발생한다`() {
         //given
         val generateRandomNumber = GenerateRandomNumberForTest(10)
@@ -29,27 +44,17 @@ class RandomTest {
         }
     }
 
-    @Test
-    fun `SecureRandom 학습 테스트`(){
-        val secureRandom = SecureRandom()
-        secureRandom.ints(1, GenerateRandomNumber.LOW_BOUND, GenerateRandomNumber.UPPER_BOUND)
-        repeat(100){
-            println(secureRandom.nextInt(9))
-        }
-
-    }
-
     class GenerateRandomNumberForTest(
         private val number: Int,
     ) : RandomUtil {
         override fun getRandomNumberAndValidation(): Int {
-            if(number <LOW_BOUND || number > UPPER_BOUND){
+            if (number < LOW_BOUND || number > UPPER_BOUND) {
                 throw IllegalArgumentException()
             }
             return number
         }
 
-        companion object{
+        companion object {
             const val LOW_BOUND = 1
             const val UPPER_BOUND = 9
         }
